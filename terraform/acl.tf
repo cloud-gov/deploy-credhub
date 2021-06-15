259522170a0e687184773e5b2b8ea0413257cec0
Andrew Burnes <andrew.burnes@gsa.gov>
2021-06-15 15:14:30 -0700

    Add concourse pages resources

--- terraform/acl.tf
+++ terraform/acl.tf
@@ -9,8 +9,13 @@ provider "credhub" {
   client_secret  = var.credhub_client_secret
 }
 
-resource "credhub_permission" "testuser_acl" {
-  path       = "/a/path/*"
+resource "credhub_user" "concourse_pages_user" {
+  name            = "concourse_pages_user"
+  username        = "concourse-pages-user"
+}
+
+resource "credhub_permission" "concourse_pages_permission" {
+  path       = "/concourse/pages/*"
   actor      = "uaa-user:dc912b22-test-4780-blah-aa5843f81868"
   operations = ["read", "write", "delete"]
 }
