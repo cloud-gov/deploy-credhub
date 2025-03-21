76328566a7696f37b7c6f09f05ed6958455da26f
Mark Boyd <mark.boyd@gsa.gov>
2025-03-21 11:23:19 -0400

    fix actor reference for Credhub opensearch CI permissions (#20)

--- terraform/acl.tf
+++ terraform/acl.tf
@@ -10,8 +10,8 @@ provider "credhub" {
 }
 
 resource "credhub_user" "concourse_pages_user" {
-  name            = "concourse_pages_user"
-  username        = "concourse-pages-user"
+  name     = "concourse_pages_user"
+  username = "concourse-pages-user"
 }
 
 resource "credhub_permission" "credhub_pages_concourse_permission" {
@@ -29,29 +29,29 @@ resource "credhub_permission" "doomsday_readonly" {
 resource "credhub_permission" "pgp" {
   path       = "/concourse/main/cloud-gov-pgp-keys"
   actor      = var.pgp_credhub_actor
-  operations = ["read","write","delete"]
+  operations = ["read", "write", "delete"]
 }
 
 resource "credhub_permission" "pages_gpg" {
   path       = "/concourse/pages/cloud-gov-pages-gpg-keys"
   actor      = var.pgp_credhub_actor
-  operations = ["read","write","delete"]
+  operations = ["read", "write", "delete"]
 }
 
 resource "credhub_permission" "opensearch_proxy_ci" {
   path       = "/concourse/main/opensearch-dashboards-cf-auth-proxy/*"
   actor      = var.opensearch_proxy_ci_credhub_actor
-  operations = ["read","write","delete"]
+  operations = ["read", "write", "delete"]
 }
 
 resource "credhub_permission" "pages_user_agent" {
   path       = "/concourse/pages/cf-build-tasks/*"
   actor      = var.pages_user_agent
-  operations = ["read","write","delete"]
+  operations = ["read", "write", "delete"]
 }
 
 resource "credhub_permission" "opensearch_ci" {
   path       = "/concourse/main/deploy-logs-opensearch/*"
-  actor      = var.opensearch_proxy_ci_credhub_actor
-  operations = ["read","write","delete"]
+  actor      = var.opensearch_ci_credhub_actor
+  operations = ["read", "write", "delete"]
 }
